091a5816012b31cbea3b5ffa8b54c883710dd5f1
Cho-SeongJu <tjdwn9753@gmail.com>
2023-10-23 15:01:11 +0900

    💄 style: footer UI 생성
    - footer UI 생성

---
+++ src/components/common/footer/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+	return (
+		<>
+			<footer className="flex flex-col items-center w-full py-12 bg-[#ABDEE6] text-white font-medium">
+				<div className="flex">
+					<div>github</div>
+					<div>로고</div>
+				</div>
+				<p>Copyright 2023 Ⓒ MentoMate Team All Rights Reserved.</p>
+			</footer>
+		</>
+	);
+};
+
+export default Footer;
