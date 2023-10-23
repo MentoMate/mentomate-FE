0bb45dc4de1cebea2c7e2448f8b753526f705d0a
Cho-SeongJu <tjdwn9753@gmail.com>
2023-10-23 19:39:12 +0900

    💄 style: 회원가입 페이지 로고 추가 및 반응형 css 적용
    - 회원가입 페이지 로고 추가
    - 반응형 css 적용

--- src/components/signUp/SignUpForm.tsx
+++ src/components/signUp/SignUpForm.tsx
@@ -12,7 +12,7 @@ const SignUpForm = () => {
 	return (
 		<form className="flex flex-col">
 			<div className="flex flex-col mb-6">
-				<label className="mb-2 text-lg font-bold">이메일</label>
+				<label className="mb-2 text-md sm:text-lg font-bold">이메일</label>
 				<div className="flex items-center mb-4">
 					<input
 						type="text"
@@ -27,14 +27,14 @@ const SignUpForm = () => {
 				</div>
 				<button
 					type="button"
-					className="py-3 bg-[#B9B9B9] rounded-md text-lg text-[#E5E5E5] font-bold"
+					className="py-3 bg-[#B9B9B9] rounded-md text-md sm:text-lg text-[#E5E5E5] font-bold"
 				>
 					이메일 인증
 				</button>
 			</div>
 			<div className="flex flex-col mb-6">
-				<label className="mb-1 text-lg font-bold">비밀번호</label>
-				<p className="mb-1 text-[#8A8A8A] text-[0.8rem]">
+				<label className="mb-1 text-md sm:text-lg font-bold">비밀번호</label>
+				<p className="mb-1 text-[#8A8A8A] text-[0.7rem] sm:text-[0.8rem]">
 					영문, 숫자, 특수문자를 포함한 8~16자리의 비밀번호를 입력해주세요.
 				</p>
 				<input
@@ -44,7 +44,9 @@ const SignUpForm = () => {
 				/>
 			</div>
 			<div className="flex flex-col mb-6">
-				<label className="mb-2 text-lg font-bold">비밀번호 확인</label>
+				<label className="mb-2 text-md sm:text-lg font-bold">
+					비밀번호 확인
+				</label>
 				<input
 					type="password"
 					className="p-3 border border-[#E5E5E5] outline-[#ABDEE6] rounded-md placeholder:text-sm"
@@ -52,7 +54,7 @@ const SignUpForm = () => {
 				/>
 			</div>
 			<div className="flex flex-col mb-6">
-				<label className="mb-2 text-lg font-bold">닉네임</label>
+				<label className="mb-2 text-md sm:text-lg font-bold">닉네임</label>
 				<input
 					type="text"
 					className="mb-4 p-3 border border-[#E5E5E5] outline-[#ABDEE6] rounded-md placeholder:text-sm"
@@ -60,13 +62,13 @@ const SignUpForm = () => {
 				/>
 				<button
 					type="button"
-					className="py-3 bg-[#B9B9B9] rounded-sm text-lg text-[#E5E5E5] font-bold"
+					className="py-3 bg-[#B9B9B9] rounded-sm text-md sm:text-lg text-[#E5E5E5] font-bold"
 				>
 					닉네임 중복확인
 				</button>
 			</div>
 			<div className="flex flex-col mb-6">
-				<label className="mb-2 text-lg font-bold">성별</label>
+				<label className="mb-2 text-md sm:text-lg font-bold">성별</label>
 				<div className="flex border rounded-md">
 					<div
 						className={`grow py-4 border-r ${
@@ -92,7 +94,7 @@ const SignUpForm = () => {
 			</div>
 			<button
 				type="submit"
-				className="py-4 bg-[#ABDEE6] text-lg font-bold text-white rounded-sm"
+				className="py-3 bg-[#ABDEE6] text-md sm:text-lg font-bold text-white rounded-sm"
 			>
 				회원가입
 			</button>
