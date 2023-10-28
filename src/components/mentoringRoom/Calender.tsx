b978a62f93a4a5535ce914a4c57084ec5174b81b
hcb1999 <hcb1999@naver.com>
2023-10-29 02:53:06 +0900

    💄 style : 멘토링 방 UI 및 세부 모달 UI - 멘토링 방 UI - 멘토링 상세 정보 모달 UI - 멘토링 일정 등록 모달 UI - 멘토링 일정 보기 모달 UI - 반응형 CSS 적용 - React-Quill 라이브러리 설치, FullCalender 라이브러리 설치

--- src/components/mentoringRoom/Calender.tsx
+++ src/components/mentoringRoom/Calender.tsx
@@ -1,58 +1,97 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import Header from "@/components/common/header/Header";
-import MentoringInfoModal from "./MentoringInfoModal";
+import interactionPlugin from "@fullcalendar/interaction";
+import DayCellContent from "./DayCellContent";
+import ScduleAddModal from "./ScduleAddModal";
+import ScduleReadModal from "./ScduleReadModal";
 import { useState } from "react";
 
-import ScduleAddModal from "./ScduleAddModal";
+const events = [
+	{
+		title: "치어리더가 되기 위한 준비 과정 그리고 노하우(1주차)",
+		start: "2023-10-09",
+		description: "오늘 주제는 ~~~~",
+		backgroundColor: "#ABDEE6",
+		borderColor: "#ABDEE6",
+	},
+	{
+		title: "이벤트 2",
+		start: "2023-11-05",
+		description: "이벤트 2 설명",
+		backgroundColor: "#ABDEE6",
+		borderColor: "#ABDEE6",
+	},
+];
+
 const MyCalendar = () => {
-	const [isModalOpen, setModalOpen] = useState(false);
+	const [hoveredDate, setHoveredDate] = useState(""); //Hover된 일정 날짜
+	const [isScduleAddModalOpen, setIsScduleAddModalOpen] = useState(false); //선택된 일정에 대한 모달 상태
+	const [scduleDate, setScduleDate] = useState(""); //선택된 일정 날짜
+	const [selectedEventDate, setSelectedEventDate] = useState(""); // 선택된 이벤트 날짜
+	const [eventTitle, setEventTitle] = useState(""); // 선택된 이벤트 제목
+	const [eventDescription, setEventDescription] = useState(""); // 선택된 이벤트 세부정보
+	const [isScduleReadModalOpen, setIsScduleReadModalOpen] = useState(false); //선택된 이벤트에 대한 모달 상태
 
-	const openModal = () => {
-		setModalOpen(true);
+	const customDayCellContent = (arg: any) => (
+		<DayCellContent
+			arg={arg}
+			hoveredDate={hoveredDate}
+			setHoveredDate={setHoveredDate}
+			events={events}
+			onClickAddEventhandler={onClickAddEventhandler}
+		/>
+	);
+
+	const onClickAddEventhandler = (e: React.MouseEvent, date: string) => {
+		e.stopPropagation();
+		setScduleDate(date);
+		setIsScduleAddModalOpen(true);
 	};
 
-	const closeModal = () => {
-		setModalOpen(false);
+	const onClickReadEventhandler = (clickInfo: any) => {
+		const { event } = clickInfo;
+		const date = new Date(event.start);
+		const formateventdate = `${date.getFullYear()}-${(date.getMonth() + 1)
+			.toString()
+			.padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
+
+		setSelectedEventDate(formateventdate);
+		setEventDescription(event.extendedProps.description);
+		setEventTitle(event.title);
+		setIsScduleReadModalOpen(true); // 모달 열기
 	};
-	const events = [
-		{
-			title: "이벤트 1",
-			start: "2023-10-26",
-			end: "2023-10-30",
-			description: "이벤트 1 설명",
-			backgroundColor: "#ABDEE6",
-			borderColor: "#ABDEE6",
-		},
-		{
-			title: "이벤트 2",
-			start: "2023-11-05",
-			description: "이벤트 2 설명",
-			backgroundColor: "#ABDEE6",
-			borderColor: "#ABDEE6",
-		},
-		// 추가 이벤트를 원하는 만큼 정의할 수 있습니다.
-	];
+
 	return (
 		<>
-			<Header />
-			<div className="relative mt-4 ml-2 mr-2">
-				<FullCalendar
-					plugins={[dayGridPlugin]}
-					initialView="dayGridMonth"
-					height="100vh"
-					events={events}
-					headerToolbar={{
-						start: "title",
-						center: "",
-						end: "prev next",
-					}}
-					locale="ko"
-				/>
-				<MentoringInfoModal />
+			<FullCalendar
+				plugins={[interactionPlugin, dayGridPlugin]}
+				initialView="dayGridMonth"
+				height="100vh"
+				events={events}
+				headerToolbar={{
+					start: "title",
+					center: "",
+					end: "prev next",
+				}}
+				locale="ko"
+				dayCellContent={customDayCellContent} // 날짜 셀의 모양과 동작을 제어
+				eventClick={onClickReadEventhandler} // 이벤트 클릭 핸들러 연결
+			/>
 
-				<ScduleAddModal />
-			</div>
+			{isScduleAddModalOpen && (
+				<ScduleAddModal //일정 추가 시 모달
+					formattedDate={scduleDate}
+					closeModal={() => setIsScduleAddModalOpen(false)}
+				/>
+			)}
+			{isScduleReadModalOpen && (
+				<ScduleReadModal //일정 보기 시 모달
+					formattedDate={selectedEventDate}
+					closeModal={() => setIsScduleReadModalOpen(false)}
+					eventText={eventTitle}
+					eventDescription={eventDescription}
+				/>
+			)}
 		</>
 	);
 };
--- src/components/mentoringRoom/ScduleReadModal.tsx
+++ src/components/mentoringRoom/ScduleReadModal.tsx
@@ -3,13 +3,16 @@ import { ReactComponent as Close } from "@/assets/svg/close.svg";
 import ReactQuill from "react-quill";
 import { quillModules } from "@/modules/quillModules";
 import "react-quill/dist/quill.snow.css";
-import { IScduleAddModalProps } from "@/types/scduleaddmodalprop";
-const ScduleAddModal: React.FC<IScduleAddModalProps> = ({
+import { IScduleReadModalProps } from "@/types/scdulereadmodalprop";
+
+const ScduleReadModal: React.FC<IScduleReadModalProps> = ({
 	formattedDate,
 	closeModal,
+	eventText,
+	eventDescription,
 }) => {
-	const [text, setText] = useState("");
-
+	const [descriptionText, setDescriptionText] = useState(eventDescription);
+	const [titleText, setTitleText] = useState(eventText);
 	return (
 		<>
 			<div className="fixed inset-0 flex items-center  justify-center z-20 ">
@@ -17,7 +20,6 @@ const ScduleAddModal: React.FC<IScduleAddModalProps> = ({
 				<div className="z-10 bg-white p-8 rounded-lg mt-20 ">
 					{/*모달 전체 박스*/}
 					<div className="flex justify-between items-center  w-[15rem] lg:w-[40rem]">
-						<h2 className="text-sm lg:text-lg font-semibold mb-2">일정 추가</h2>
 						<Close onClick={closeModal} width={20} height={20} />
 					</div>
 
@@ -29,10 +31,12 @@ const ScduleAddModal: React.FC<IScduleAddModalProps> = ({
 							type="text"
 							className="my-1 p-4 border border-black-200 rounded-md placeholder:text-sm focus:outline-main-color"
 							placeholder="제목을 입력해주세요"
+							onChange={(e) => setTitleText(e.target.value)}
+							value={titleText}
 						/>
 						<ReactQuill
-							value={text}
-							onChange={setText}
+							value={descriptionText}
+							onChange={setDescriptionText}
 							modules={quillModules}
 							className="h-[10rem] lg:h-[30rem] mb-12"
 							placeholder="설명"
@@ -41,7 +45,7 @@ const ScduleAddModal: React.FC<IScduleAddModalProps> = ({
 							type="submit"
 							className=" px-3 py-4 bg-main-color rounded-md font-bold text-white text-lg"
 						>
-							추가하기
+							저장하기
 						</button>
 					</form>
 
@@ -51,4 +55,4 @@ const ScduleAddModal: React.FC<IScduleAddModalProps> = ({
 		</>
 	);
 };
-export default ScduleAddModal;
+export default ScduleReadModal;
