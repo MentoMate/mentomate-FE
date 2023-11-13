0b712e6e7438b6d40c83a2ae967386493ca724de
Cho-SeongJu <tjdwn9753@gmail.com>
2023-11-13 20:49:29 +0900

    🎨 improve: useInput Parameter type 추가 및 미사용코드 삭제
    - useInput Parameter type 추가 및 미사용코드 삭제

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,24 +1,6 @@
-import { alertHandler } from "@/utils/alert";
 import { getCookie } from "@/utils/cookies";
-import axios, {
-	AxiosError,
-	AxiosInstance,
-	AxiosRequestConfig,
-	AxiosResponse,
-} from "axios";
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
 import { useEffect, useState } from "react";
-
-interface IErrorStatus {
-	[key: number]: string;
-}
-
-const errorStatus: IErrorStatus = {
-	400: "잘못된 요청입니다. 다시 시도해주세요.",
-	401: "인증 실패하였습니다.",
-	403: "접근 권한이 없습니다.",
-	404: "찾을 수 없는 경로입니다.",
-	500: "서버에 오류가 발생하였습니다.",
-};
 
 const useAxios = () => {
 	const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -34,28 +16,6 @@ const useAxios = () => {
 		},
 	});
 
-	const onResponse = (response: AxiosResponse) => {
-		return response;
-	};
-
-	const onErrorResponse = (error: AxiosError | Error) => {
-		if (axios.isAxiosError(error)) {
-			if (error && error.response) {
-				alertHandler(
-					"error",
-					error.response.data
-						? error.response?.data
-						: errorStatus[error.response.status],
-				);
-				return;
-			}
-		}
-	};
-
-	// const setUpInterceptors = (axiostInstance: AxiosInstance) => {
-	// 	axiostInstance.interceptors.response.use(onResponse, onErrorResponse);
-	// };
-
 	const fetchDataUseAxios = async (
 		type: string,
 		configParams: AxiosRequestConfig,
@@ -63,12 +23,10 @@ const useAxios = () => {
 		setIsLoading(true);
 		try {
 			if (type === "defaultAxios") {
-				// setUpInterceptors(defaultAxios);
 				const response = await defaultAxios.request(configParams);
 				return response;
 			}
 			if (type === "useTokenAxios") {
-				// setUpInterceptors(useTokenAxios);
 				const response = await useTokenAxios.request(configParams);
 				return response;
 			}
@@ -81,10 +39,6 @@ const useAxios = () => {
 			setIsLoading(false);
 		}
 	};
-
-	useEffect(() => {
-		console.log(isLoading);
-	}, [isLoading]);
 
 	return { isLoading, fetchDataUseAxios };
 };
