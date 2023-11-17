c7dd84b2afed808366ff9447786d130d21be839c
Cho-SeongJu <tjdwn9753@gmail.com>
2023-11-18 03:17:29 +0900

    💄 style: 채팅 모달창 css 수정
    - 채팅 모달창 css 수정

--- src/components/common/chatAndScrollTop/chat/ChatModalContainer.tsx
+++ src/components/common/chatAndScrollTop/chat/ChatModalContainer.tsx
@@ -34,9 +34,9 @@ const ChatModalContainer = ({ client, chatList }: IChatListClientProps) => {
 
 	return (
 		<div
-			className={`flex flex-col absolute top-[-39rem] left-[-20rem] w-[23rem] h-[38rem] bg-black-100 border border-gray-100 rounded-3xl shadow-xl opacity-0 ${
-				isOpenChat && "opacity-100"
-			} transition duration-700 transform z-[102]`}
+			className={`${
+				isOpenChat ? "flex flex-col" : "hidden"
+			} absolute top-[-39rem] left-[-20rem] w-[23rem] h-[38rem] bg-black-100 border border-gray-100 rounded-3xl shadow-xl z-[102]`}
 		>
 			<div className="mt-2 flex justify-between items-center">
 				<Logo width={130} height={70} className="ml-8" />
