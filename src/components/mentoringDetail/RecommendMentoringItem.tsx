b98f833966222b6c8f8fab4da6e973c088256f6a
Cho-SeongJu <tjdwn9753@gmail.com>
2023-11-19 00:44:20 +0900

    🎨 improve: 이런 멘토링 어때요 조회 코드 수정
    - 이런 멘토링 어때요 조회 코드 수정

--- src/components/mentoringDetail/RecommendMentoringItem.tsx
+++ src/components/mentoringDetail/RecommendMentoringItem.tsx
@@ -1,16 +1,38 @@
-const RecommendMentoringItem = () => {
+interface IRecommendMentoring {
+	readonly mentoringId: number;
+	readonly title: string;
+	readonly content: string;
+	readonly startDate: string;
+	readonly endDate: string;
+	readonly numberOfPeople: number;
+	readonly amount: number;
+	readonly status: string;
+	readonly category: string;
+	readonly uploadUrl: string;
+	readonly uploadFolder: string;
+	readonly countWatch: number;
+	readonly rating: number;
+	readonly name: string;
+	readonly registerDate: string;
+	readonly updateDate: string;
+	readonly deleteDate: string | null;
+}
+
+interface IProps {
+	readonly recommendMentoring: IRecommendMentoring;
+}
+
+const RecommendMentoringItem = ({ recommendMentoring }: IProps) => {
 	return (
 		<div className="my-4 w-[13rem] lg:w-[13rem] md:w-[10rem]">
 			<div className="w-[13rem] lg:w-[13rem] md:w-[10rem] lg:h-[15rem] md:h-[12rem]">
 				<img
-					src="src/assets/image/sample.jpg"
+					src={recommendMentoring.uploadUrl}
 					alt="asd"
 					className="w-full h-full object-cover"
 				/>
 			</div>
-			<div className="my-2 title-overflow">
-				프론트엔드 실무 가이드 요구사항 분석과 적정 기술
-			</div>
+			<div className="my-2 title-overflow">{recommendMentoring.title}</div>
 		</div>
 	);
 };
