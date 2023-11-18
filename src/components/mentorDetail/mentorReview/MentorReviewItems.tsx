3ef67c3b1b2c8c568d431e184f3aa9d90ad952fe
Cho-SeongJu <tjdwn9753@gmail.com>
2023-11-18 23:31:03 +0900

    🚩 feat: 이런 멘토링은 어때요, 후기 API 추가
    - 이런 멘토링은 어때요, 후기 API 추가

--- src/components/mentorDetail/mentorReview/MentorReviewItems.tsx
+++ src/components/mentorDetail/mentorReview/MentorReviewItems.tsx
@@ -1,15 +1,19 @@
+import { IReview } from "@/interface/MentoringDetail";
 import { ReactComponent as Star } from "@assets/svg/star.svg";
 
-const MentorReviewItems = () => {
+interface IProps {
+	readonly review: IReview;
+}
+
+const MentorReviewItems = ({ review }: IProps) => {
 	return (
 		<div className="mb-4 p-4 border border-black-200 rounded-md">
 			<div className="flex items-center mb-1">
 				<Star width={20} height={20} />
-				<div className="ml-2 font-semibold">4.9</div>
+				<div className="ml-2 font-semibold">{review.rating}</div>
 			</div>
 			<div className="lg:w-[17.5rem] md:w-[23rem] w-full md:text-md text-sm review-overflow">
-				멘토링을 하고 완벽하게 할 수 있게 되었어요 너무 좋은 멘토링이였습니다.
-				다음에 또 하게 될 수 있다면 꼭 참여하고 싶어요 그리고 ......
+				{review.comment}
 			</div>
 		</div>
 	);
