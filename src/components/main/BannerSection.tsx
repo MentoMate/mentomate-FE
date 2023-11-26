d2847df085180bb6b2c73a694e1e47597034d9f0
Cho-SeongJu <tjdwn9753@gmail.com>
2023-11-27 03:03:34 +0900

    💄 style: 메인 영역 css 수정
    - 메인 영역 css 수정

--- src/components/main/BannerSection.tsx
+++ src/components/main/BannerSection.tsx
@@ -6,12 +6,12 @@ const BannerSection = () => {
 			<div className="flex flex-col justify-center items-center relative pt-6 pb-12 w-full bg-white">
 				<div className="flex items-center">
 					<Logo className="w-44 h-24" />
-					<p className="ml-4 text-2xl font-bold">
+					<p className="ml-4 sm:text-2xl text-base font-bold">
 						멘토 & 멘티 매칭 플랫폼 - MENTORMATE :)
 					</p>
 				</div>
 				<img
-					className="w-[30rem] lg:w-[60rem] md:w-[40rem] h-[20rem] object-cover"
+					className="w-[20rem] lg:w-[60rem] md:w-[40rem] h-[20rem] object-cover"
 					src="/src/assets/image/mentorMateBanner.jpg"
 					alt="card image"
 				/>
